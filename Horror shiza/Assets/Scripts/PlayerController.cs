9d7d459d79cef97510062d4ae2f5edd267813c3e
NOISE77 <andreymir1212@gmail.com>
2021-09-06 21:41:07 +0300

    F
    F

--- Horror shiza/Assets/Scripts/PlayerController.cs
+++ Horror shiza/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Скорость перемещения персонажа")]
+    public float speed = 7f;
+
+    private void Update()
     {
-        
+        GetInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetInput()
     {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.localPosition += transform.forward * speed * Time.deltaTime;
+        }
         
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.localPosition += -transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localPosition += -transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localPosition += transform.right * speed * Time.deltaTime;
+        }
     }
 }
+
